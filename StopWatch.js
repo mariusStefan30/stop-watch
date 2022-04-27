cfc14c7bcef884e1905de77bcd5f2da60eabd073
Marius Stefan <54823269+mariusStefan30@users.noreply.github.com>
2022-04-27 11:24:06 +0100

    different implementation
    better

--- StopWatch.js
+++ StopWatch.js
@@ -1,50 +1,91 @@
-window.onload = function (){
-    let seconds = 00;
-    let tens = 00;
-    let addSeconds = document.getElementById("seconds");
-    let addTens = document.getElementById("tens");
-    let start = document.getElementById("start");
-    let stop = document.getElementById("stop");
-    let reset = document.getElementById("reset");
-    let interval;
+let tens = 00;
+let seconds = 00;
+let minutes = 00;
+let hours = 00;
+let appendTens = document.getElementById("tens");
+let appendSeconds = document.getElementById("seconds");
+let appendMinutes = document.getElementById("minutes");
+let appendHours = document.getElementById("hours");
+let startClock = document.getElementById("start");
+let stopClock = document.getElementById("stop");
+let resetClock = document.getElementById("reset");
+let interval; // to store timer values
 
-    butonDeStart.onclick = function() {
-        clearInterval(interval);
-        interval = setInterval(start, 10);
+
+//implement timing
+function startTimer() {
+    //tens
+    tens++;
+    //output 0 in front
+    if (tens <= 9) {
+        appendTens.innerHTML = "0" + tens;
     }
 
-    butonDeStop.onclick = function() {
-        clearInterval(interval);
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
     }
 
-    reset.onclick = function() {
-        clearInterval(interval);
-        tens = "00";
-        seconds = "00";
-        addTens.innerHTML = tens;
-        addSeconds.innerHTML = seconds;
+    if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = seconds;
+        tens = 0;
+        appendTens.innerHTML = 0;
     }
 
-    function start () {
-        tens++;
-
-        if(tens < 6){
-            addTens.innerHTML = "0" + tens;
-        }
-
-        if(tens > 6){
-            adaugaZecimi.innerHTML = zecimi;
-        }
-
-        if(tens > 60)
-            seconds++;
-            addSeconds.innerHTML = "0" + secunde;
-            tens = 0;
-            addTens.innerHTML = "0" + 0;
-        }
-
-        if(secunde > 6){
-            addSeconds.innerHTML = seconds;
-        }
+    //seconds
+    //output 0 in front
+    if (seconds <= 9) {
+        appendSeconds.innerHTML = "0" + seconds;
     }
+
+    if (seconds > 59) {
+        minutes++;
+        appendMinutes.innerHTML = minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = 0;
+        tens = 0;
+        appendTens.innerHTML = 0;
+    }
+
+    //minutes
+    //output 0 in front
+    if (minutes <= 9) {
+        appendMinutes.innerHTML = "0" + minutes;
+    }
+
+    if (minutes > 59) {
+        hours++;
+        appendHours.innerHTML = hours;
+    }
+
+    //hours
+    //output 0 in front
+    if (hours <= 9) {
+        appendHours.innerHTML = "0" + hours;
+    }
+
+    if (hours > 60) {
+        appendHours.innerHTML = hours;
+    }
+}
+
+
+startClock.onclick = function () {
+    interval = setInterval(startTimer, 10);
+}
+
+stopClock.onclick = function () {
+    clearInterval(interval);
+}
+
+resetClock.onclick = function () {
+    clearInterval(interval);
+    tens = "00";
+    seconds = "00";
+    minutes = "00";
+    hours = "00";
+    appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
+    appendHours.innerHTML = hours;
 }
