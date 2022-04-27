3c87894ea194e658cfcd82116e4fba5ff27f6e53
Marius Stefan <54823269+mariusStefan30@users.noreply.github.com>
2022-04-27 09:14:20 +0100

    Update StopWatch.js
    update some variable bame

--- StopWatch.js
+++ StopWatch.js
@@ -1,51 +1,50 @@
 window.onload = function (){
-    let secunde = 00;
-    let zecimi = 00;
-    let adaugaSecunde = document.getElementById("secunde");
-    let adaugaZecimi = document.getElementById("zecimi");
-    let butonDeStart = document.getElementById("butonDeStart");
-    let butonDeStop = document.getElementById("butonDeStop");
-    let butonDeResetare = document.getElementById("butonDeResetare");
+    let seconds = 00;
+    let tens = 00;
+    let addSeconds = document.getElementById("seconds");
+    let addTens = document.getElementById("tens");
+    let start = document.getElementById("start");
+    let stop = document.getElementById("stop");
+    let reset = document.getElementById("reset");
     let interval;
 
     butonDeStart.onclick = function() {
         clearInterval(interval);
-        interval = setInterval(startTimp, 10);
+        interval = setInterval(start, 10);
     }
 
     butonDeStop.onclick = function() {
         clearInterval(interval);
     }
 
-    butonDeResetare.onclick = function() {
+    reset.onclick = function() {
         clearInterval(interval);
-        zecimi = "00";
-        secunde = "00";
-        adaugaZecimi.innerHTML = zecimi;
-        adaugaSecunde.innerHTML = secunde;
+        tens = "00";
+        seconds = "00";
+        addTens.innerHTML = tens;
+        addSeconds.innerHTML = seconds;
     }
 
-    function startTimp () {
-        zecimi++;
+    function start () {
+        tens++;
 
-        if(zecimi < 9){
-            adaugaZecimi.innerHTML = "0" + zecimi;
+        if(tens < 6){
+            addTens.innerHTML = "0" + tens;
         }
 
-        if(zecimi > 9){
+        if(tens > 6){
             adaugaZecimi.innerHTML = zecimi;
         }
 
-        if(zecimi > 99) {
-            console.log("secunde");
-            secunde++;
-            adaugaSecunde.innerHTML = "0" + secunde;
-            zecimi = 0;
-            adaugaZecimi.innerHTML = "0" + 0;
+        if(tens > 60)
+            seconds++;
+            addSeconds.innerHTML = "0" + secunde;
+            tens = 0;
+            addTens.innerHTML = "0" + 0;
         }
 
-        if(secunde > 9){
-            adaugaSecunde.innerHTML = secunde;
+        if(secunde > 6){
+            addSeconds.innerHTML = seconds;
         }
     }
 }
